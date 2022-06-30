13b937d9b8699fbff9be783b48281444fbbe919d
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-06-30 14:08:31 -0700

    add second test

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
+const { expect } = require("chai");
+
 describe("todo", () => {
   it("todo", () => {
     expect(aVariable).to.equal("hello");
+  });
+
+  it("todo", () => {
+    expect(anotherVariable).to.equal("goodbye");
   });
 });
